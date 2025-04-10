23aa7ebaafa85d82503275b779d8183c49542963
jeanrrf <rosso.jeandasilva@gmail.com>
2025-04-10 12:55:24 -0300

    feat: add webpack configuration for module bundling and asset management

---
+++ product-showcase-frontend/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.jsx',
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.web.jsx', '.mjs', '.web.js'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+};
